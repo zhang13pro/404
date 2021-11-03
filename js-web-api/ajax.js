a77f9b0121e2916df41958e642df3916dde47057
zhang13pro <lexzhang13@qq.com>
2021-11-03 20:10:31 +0800

    网络

--- js-web-api/ajax.js
+++ js-web-api/ajax.js
@@ -1,40 +1,35 @@
-// const xhr = new XMLHttpRequest()
-// xhr.open('GET', '/data/test.json', true)
-// xhr.onreadystatechange = function () {
-//     if (xhr.readyState === 4) {
-//         if (xhr.status === 200) {
-//             // console.log(
-//             //     JSON.parse(xhr.responseText)
-//             // )
-//             alert(xhr.responseText)
-//         } else if (xhr.status === 404) {
-//             console.log('404 not found')
-//         }
-//     }
-// }
-// xhr.send(null)
+const xhr = new XMLHttpRequest();
+xhr.onreadystatechange = function () {
+  if (xhr.readyState === 4) {
+    if (xhr.status === 200) {
+      console.log(JSON.parse(xhr.responseText));
+    } else if (xhr.status === 404) {
+      console.log("404 not found");
+    }
+  }
+};
+xhr.open("GET", "/data/test.json", true);
+xhr.send(null);
 
 function ajax(url) {
-    const p = new Promise((resolve, reject) => {
-        const xhr = new XMLHttpRequest()
-        xhr.open('GET', url, true)
-        xhr.onreadystatechange = function () {
-            if (xhr.readyState === 4) {
-                if (xhr.status === 200) {
-                    resolve(
-                        JSON.parse(xhr.responseText)
-                    )
-                } else if (xhr.status === 404 || xhr.status === 500) {
-                    reject(new Error('404 not found'))
-                }
-            }
+  const p = new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET", url, true);
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4) {
+        if (xhr.status === 200) {
+          resolve(JSON.parse(xhr.responseText));
+        } else if (xhr.status === 404 || xhr.status === 500) {
+          reject(new Error("404 not found"));
         }
-        xhr.send(null)
-    })
-    return p
+      }
+    };
+    xhr.send(null);
+  });
+  return p;
 }
 
-const url = '/data/test.json'
+const url = "/data/test.json";
 ajax(url)
-.then(res => console.log(res))
-.catch(err => console.error(err))
+  .then((res) => console.log(res))
+  .catch((err) => console.error(err));
---
+++ js-web-api/http.md
@@ -0,0 +1,5 @@
+## Restful API
+
+- 一种新的 API 设计方法（早已推广使用）
+- 传统 API 设计：把每个 URL 当作一个功能
+- Restful API 设计：把每个 URL 当作一个唯一的资源
