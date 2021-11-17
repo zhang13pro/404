8d7cb75a33d77763d46ec72d2cb0d0083f80a710
Lex <zhang13pro@qq.com>
2021-11-17 09:45:50 +0800

    create中的new

--- 手写代码/Object.create()-polyfill.js
+++ 手写代码/Object.create()-polyfill.js
@@ -5,6 +5,10 @@ if (!Object.create) {
   Object.create = function (obj) {
     function F() {}
     F.prototype = obj;
+    // new 1、创建新对象
+    // 2、新对象的__proto__指向构造函数的prototype
+    // 3、执行构造函数代码 this指向新对象
+    // 4、判断返回值 引用类型?该引用:新对象
     return new F();
   };
 }
