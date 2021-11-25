2ff7939e178a60c0495ce33cfd7d0c93a15e87a7
Lex <zhang13pro@qq.com>
2021-11-25 11:28:42 +0800

    防抖应用

--- 手写代码/防抖.js
+++ 手写代码/防抖.js
@@ -1,3 +1,12 @@
+var count = 1;
+var container = document.getElementById("container");
+
+function getUserAction() {
+  container.innerHTML = count++;
+}
+
+container.onmousemove = debounce(getUserAction, 1000);
+
 function debounce(fn, delay) {
   let timer = null;
   return function () {
